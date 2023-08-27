46dba8ace717b5fe2717c5412ea539e192b67597
Suraj Kumar <sraj15082003@gmail.com>
2023-08-27 13:48:09 +0530

    Second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000', // Set the specific origin of your frontend
+    origin: 'https://payment-frontend-seven.vercel.app', // Set the specific origin of your frontend
     credentials: true // Allow sending credentials (cookies)
 }));
 
